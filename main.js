41b36d8ea7980d7cd65591c4ace5713966b3aa03
xizzana <xizanishvili.99@gmail.com>
2021-03-27 14:24:10 +0400

    enhance

---
+++ main.js
@@ -0,0 +1,13 @@
+var progress = document.getElementById('progressbar');
+var totalheight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function(){
+	var progressheight = (window.pageYOffset / totalheight) * 100;
+	progress.style.height = progressheight + "%";
+}
+
+
+
+window.addEventListener("load", function(){
+	var loader = document.querySelector(".centerdiv")
+	loader.className += " hidden";
+})
