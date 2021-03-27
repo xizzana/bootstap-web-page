c04d0fc903ca9b81b2df9b92f554f8c1e2b91621
xizzana <xizanishvili.99@gmail.com>
2021-03-27 22:35:36 +0400

    .

--- main.js
+++ main.js
@@ -5,26 +5,41 @@ window.onscroll = function(){
 	progress.style.height = progressheight + "%";
 }
 
-
-
 window.addEventListener("load", function(){
 	var loader = document.querySelector(".centerdiv")
 	loader.className += " hidden";
 })
 
-window.addEventListener('scroll', ()=>{
-	if(window.pageYOffset > 500){
-		console.log('yleo');
-		const counter = document.querySelector('.counter');
-		let count = 1;
-			setInterval( ()=>{
-				if(count < 500){
-					count++;
-					counter.innerHTML = count;
-				}
-			} , 10)
-	}
-})
+var counter = document.querySelectorAll(".counter");
+for(var i = 0; i < counter.length; i++) {
+	window.addEventListener('scroll', ()=>{
+		if(window.pageYOffset > 500){
+			let count = 1;
+				setInterval( ()=>{
+					if(count < 500){
+						count++;
+						counter[0].innerHTML = count;
+					}
+				} , 50)
+
+				setInterval( function(){
+					if(count < 1300){
+						count++;
+						counter[1].innerHTML = count;
+					}
+				} , 10)
+				setInterval( function(){
+					if(count < 1300){
+						count++;
+						counter[2].innerHTML = count;
+					}
+				} , 10)
+		} return;
+
+	});
+
+}
+
 
 
 
