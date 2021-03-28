e82d85eb5003d6ba05b94777edd9bb42a9400f52
xizzana <xizanishvili.99@gmail.com>
2021-03-28 12:45:38 +0400

    .

--- scroll.js
+++ scroll.js
@@ -4,18 +4,7 @@ $(document).ready(function() {
       e.preventDefault();
       $('body,html').animate({
         scrollTop: $(this.hash).offset().top
-      }, 700 );
+      }, 1000 );
     });
-    $(window).scroll(function() {
-      var scrollbarLocation = $(this).scrollTop();
-      scrollLink.each(function() {
-        var sectionOffset = $(this.hash).offset().top - 20;
-        if ( sectionOffset <= scrollbarLocation ) {
-          $(this).parent().addClass('active');
-          $(this).parent().siblings().removeClass('active');
-        }
-      })
-      
-    })
     
   })
